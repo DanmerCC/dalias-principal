cd962b3758f70b7f9a9ab51056d63ef647eaadfc
pablo gutierrez <pabloyucragutierrez@gmail.com>
2025-12-18 12:50:53 -0500

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/inicio' 
+  }
+];
